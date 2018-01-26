9dbaedd4b29edb5270dc2c559d70a108112d0e79
An Nuyen <annuyen@gmail.com>
2018-01-26 19:42:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-// Code your solutions in this file
-const badges = ["Joe", "Gabe"];
+function printBadges (names) {
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Welcome ${names[i]}! You are employee #${i+1}.`);
+ }
 
-function printBadges(names){
-  for (let i=0; i < names.length; i++){
-    console.log(`Welcome ${badges[i]}! You are employee #${i+1}.`);
-  }
-  return names;
+ return names;
 }
 
-function tailsNeverFails() {
-
-  let tails = 0;
-  while (Math.random() <= .5) {
-    tails++;
-  }
-  return `You got ${tails} tails in a row!`;
+function coinFlip () {
+  return Math.random() >= 0.5;
 }
 
-// hello 
+function tailsNeverFails () {
+  let i = 0;
+  while (coinFlip()) {
+    i++;
+  }
+  return `You got ${i} tails in a row!`;
+}
